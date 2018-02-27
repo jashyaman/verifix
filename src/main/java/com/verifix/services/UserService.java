13dc32b5fde437d8e86303962b372e25b49823a3
Shyam Raman <24shyam7@gmail.com>
2018-02-26 22:54:24 -0500

    src

--- src/main/java/com/verifix/services/UserService.java
+++ src/main/java/com/verifix/services/UserService.java
@@ -2,12 +2,17 @@ package com.verifix.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.verifix.model.User;
+import com.verifix.repository.UserRepository;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	UserRepository userRepository;
 	
 	public UserService() {
 		// TODO Auto-generated constructor stub

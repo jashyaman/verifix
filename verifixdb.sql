f6b40b7984eada1a4bf060eacc72535bfb9fb121
Shyam Raman <24shyam7@gmail.com>
2018-03-03 23:12:31 -0500

    sql ddl scripts

---
+++ verifixdb.sql
@@ -0,0 +1,81 @@
+create table
+upload_log
+( upload_id int,
+upload_type text,
+filename text,
+uploader_id int,
+upload_timestamp timestamp);
+
+
+create table
+project
+( project_id int,
+project_name text,
+project_description text,
+resource_group_id int,
+project_owner_id text,
+create_timestamp timestamp,
+updater_id int,
+updater_timestamp datetime);
+
+
+create table
+resource_group
+( resource_group_id int,
+resource_group_name text,
+creator_id int,
+create_timestamp timestamp,
+updater_id int,
+updater_timestamp datetime);
+
+-- there can be only one timestamp in a record
+
+create table
+resource 
+( resource_version text,
+resource_id int,
+creator_id int,
+create_timestamp timestamp,
+updater_id int,
+updater_timestamp datetime);
+
+create table
+res_rg_map
+( res_rg_map_id int,
+resource_version text,
+resource_id int,
+resource_group_id);
+
+create table
+role
+( role_id int,
+role_name text,
+role_description text);
+
+create table
+role_group
+( role_group_id int,
+role_group_name text,
+role_id int);
+
+create table
+user
+( user_id int,
+username text,
+first_name text,
+last_name text,
+manager_id int,
+role_group_id int);
+
+create table 
+modification_log
+( log_id int,
+object_type text,
+object_modified text,
+modifier_id int
+modification_timestamp timestamp);
+
+create table
+credentials
+( username text,
+password text);

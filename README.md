7fec8b75d3203e1edfe6b3c080267fa282c05763
Shyam Raman <24shyam7@gmail.com>
2018-03-13 22:44:13 -0400

    added endpoints to README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # verifix
+${hostname}:${port}/verifix/v1
+/users/all
+/uploadlogs/all
+/projects/all
+/roles/all
+/finduserroles/{userid}
+/rolegroups/all
+/resources/all
+/resourcesgroups/all
+/resrgmap/{resgid}
+/notificationlog/all
+/credentialsvalidate
--- src/main/java/com/verifix/SpringConfig.java
+++ src/main/java/com/verifix/SpringConfig.java
@@ -11,9 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -76,6 +74,4 @@ public class SpringConfig {
         return new PersistenceExceptionTranslationPostProcessor();
     }
 
-	
-	
 }
